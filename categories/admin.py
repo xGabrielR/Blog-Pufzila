33faa62f9219a752d5047f877d33b6fedac63a8c
Gabriel Richter <75986085+xGabrielR@users.noreply.github.com>
2022-03-26 17:56:25 -0300

    Delete categories directory

--- categories/admin.py
+++
@@ -1,9 +0,0 @@
-from unicodedata import category
-from django.contrib import admin
-from .models import Category
-
-@admin.register(Category)
-class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name',)
-    list_editable = ('name',)
-    list_display_links = ('id',)
